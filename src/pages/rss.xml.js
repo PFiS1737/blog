079a6882e38c26da03b27ea312fe530244a17458
PFiS1737 <pfis1737@gmail.com>
2025-11-04 15:41:55 +0800

    style: formatted by biomejs

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,16 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { getCollection } from "astro:content"
+import rss from "@astrojs/rss"
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts"
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const posts = await getCollection("blog")
+  return rss({
+    description: SITE_DESCRIPTION,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.id}/`,
+    })),
+    site: context.site,
+    title: SITE_TITLE,
+  })
 }
