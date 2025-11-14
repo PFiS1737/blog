6d583b8387b70556706b0195304c69ad01132751
PFiS1737 <pfis1737@gmail.com>
2025-11-14 17:08:57 +0800

    deps: add tailwindcss support

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,16 @@
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
-import config from "@/site.config"
+import config from "./src/site.config"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap()],
   site: config.site,
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
+
