d66f028d54fd618790a22439631e38ab760d6df8
PFiS1737 <pfis1737@gmail.com>
2025-11-14 15:41:51 +0800

    feat: update content config
    Rename collection `blog` -> `posts`

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,27 @@
 import { defineCollection, z } from "astro:content"
 import { glob } from "astro/loaders"
+import config from "@/site.config"
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
+const posts = defineCollection({
+  loader: glob({ base: "./src/content/posts", pattern: "**/[^_]*.{md,mdx}" }),
   schema: ({ image }) =>
+    // biome-ignore assist/source/useSortedKeys: better readability
     z.object({
-      description: z.string(),
-      heroImage: image().optional(),
-      // Transform string to Date object
-      pubDate: z.coerce.date(),
       title: z.string(),
+      author: z.string().optional().default(config.author),
+      description: z.string().optional(),
+
+      pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
+
+      heroImage: image().optional(),
+
+      tags: z.array(z.string()).optional().default(["others"]),
+      series: z.string().optional(),
+      featured: z.boolean().optional().default(false),
+
+      toc: z.boolean().optional().default(true),
     }),
 })
 
-export const collections = { blog }
+export const collections = { posts }
