2ce0f4d9c8e4a6c8aae3d5d2973e71441bec5c90
PFiS1737 <pfis1737@gmail.com>
2025-11-14 21:32:14 +0800

    feat: config for remark and rehype

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,40 @@
+import { rehypeHeadingIds } from "@astrojs/markdown-remark"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import rehypeExternalLinks from "rehype-external-links"
+import rehypeKatex from "rehype-katex"
+import remarkGemoji from "remark-gemoji"
+import remarkGithubAlerts from "remark-github-alerts"
+import remarkMath from "remark-math"
+import remarkToc from "remark-toc"
 import config from "./src/site.config"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap()],
+  markdown: {
+    rehypePlugins: [
+      [rehypeHeadingIds, { headingIdCompat: true }],
+      [
+        rehypeAutolinkHeadings,
+        { behavior: "append", content: { type: "text", value: "#" } },
+      ],
+      [
+        rehypeExternalLinks,
+        {
+          rel: ["noreferrer", "noopener"],
+          target: "_blank",
+        },
+      ],
+      rehypeKatex,
+    ],
+    remarkPlugins: [remarkToc, remarkGithubAlerts, remarkMath, remarkGemoji],
+  },
   site: config.site,
-
   vite: {
     plugins: [tailwindcss()],
   },
 })
-
