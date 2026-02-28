11ac9284417d2adc9dd96b68c3e7eaa07503e944
PFiS1737 <pfis1737@gmail.com>
2026-02-28 16:21:42 +0800

    fix: add types for site configs

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,8 +1,37 @@
 /** biome-ignore-all assist/source/useSortedKeys: better readability */
 
-import { MailIcon, MarkGithubIcon } from "@primer/octicons-react"
+import { MailIcon, MarkGithubIcon, type Icon as Octicon, type IconProps as OcticonProps } from "@primer/octicons-react"
+import type { SvgComponent } from "astro/types"
 import IconBilbil from "@/assets/icons/bilibili.svg"
 import IconSteam from "@/assets/icons/steam.svg"
+
+type SiteConfig = {
+  site: string
+
+  title: string
+  author: string
+  description: string
+
+  github: string
+  repo: string
+  branch: string
+
+  socialLinks: {
+    title: string
+    href: string
+    icon: SvgComponent | Octicon
+    octiconProps?: OcticonProps
+  }[]
+
+  appearance: {
+    postsPerPage: number
+    recentPostsCount: number
+  }
+
+  giscus: {
+    enabled: boolean
+  }
+}
 
 export default {
   site: "https://pfis1737.github.io",
@@ -20,43 +49,38 @@ export default {
       title: "GitHub",
       href: "https://github.com/pfis1737",
       icon: MarkGithubIcon,
-      iconProps: { size: 24 },
+      octiconProps: { size: 24 },
     },
     {
       title: "Mail",
       href: "mailto:pfis1737@gmail.com",
       icon: MailIcon,
-      iconProps: { size: 16 }, // This is only the octicon style. The icons are always rendered with 'size-6' (24px).
+      octiconProps: { size: 16 }, // This is only the octicon variant. The icons are always rendered with 'size-6' (24px).
     },
     // {
     //   title: "X",
     //   href: "https://x.com/pfis1737",
     //   icon: IconX,
-    //   iconProps: { width: 24, height: 24 },
     // },
     // {
     //   title: "Telegram",
     //   href: "https://t.me/pfis1737",
     //   icon: IconTelegram,
-    //   iconProps: { width: 24, height: 24 },
     // },
     // {
     //   title: "Discord",
     //   href: "https://discord.com/users/pfis1737",
     //   icon: IconDiscord,
-    //   iconProps: { width: 24, height: 24 },
     // },
     {
       title: "Steam",
       href: "https://steamcommunity.com/id/pfis1737",
       icon: IconSteam,
-      iconProps: { width: 24, height: 24 },
     },
     {
       title: "bilibili",
       href: "https://space.bilibili.com/470803955",
       icon: IconBilbil,
-      iconProps: { width: 24, height: 24 },
     },
   ],
 
@@ -68,4 +92,4 @@ export default {
   giscus: {
     enabled: true,
   },
-} as const
+} as SiteConfig
